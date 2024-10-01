b25d8a50751a44e4802581062ed5238b508c3158
thiagoclrss <thiagocolaress77@gmail.com>
2024-09-30 22:58:24 -0300

    db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"ApiGo/controller"
+	"ApiGo/db"
+	"ApiGo/usecase"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := gin.Default()
+
+	dbConnection, err := db.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	ProductUsecase := usecase.NewProductUsecase()
+
+	ProductController := controller.NewProductController(ProductUsecase)
 
 	server.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -11,5 +25,6 @@ func main() {
 		})
 	})
 
+	server.GET("/products", ProductController.GetProducts)
 	server.Run(":8000")
 }
