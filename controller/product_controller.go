023e9e7c66bb449dc9d73e6c2afec3335cd2dacc
thiagoclrss <thiagocolaress77@gmail.com>
2024-10-08 11:01:22 -0300

    creating docker image of the application

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -5,6 +5,7 @@ import (
 	"ApiGo/usecase"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 type ProductController struct {
@@ -42,4 +43,39 @@ func (p *ProductController) CreateProduct(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, insertedProduct)
+}
+
+func (p *ProductController) GetProductById(ctx *gin.Context) {
+	id := ctx.Param("productId")
+	if id == "" {
+		response := model.Response{
+			Message: "Id do produto não pode ser nulo!",
+		}
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	productId, err := strconv.Atoi(id)
+	if err != nil {
+		response := model.Response{
+			Message: "Id do produto precisa ser um número!",
+		}
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	product, err := p.productUsecase.GetProductById(productId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	if product == nil {
+		response := model.Response{
+			Message: "Produto não foi encontrado na base de dados.",
+		}
+		ctx.JSON(http.StatusNotFound, response)
+		return
+	}
+	ctx.JSON(http.StatusOK, product)
 }
--- repository/product_repository.go
+++ repository/product_repository.go
@@ -64,3 +64,25 @@ func (pr *ProductRepository) CreateProduct(product model.Product) (int, error) {
 	query.Close()
 	return id, nil
 }
+
+func (pr *ProductRepository) GetProductById(id_product int) (*model.Product, error) {
+	query, err := pr.connection.Prepare("SELECT * FROM product WHERE id = $1")
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var product model.Product
+
+	err = query.QueryRow(id_product).Scan(
+		&product.ID, &product.Name, &product.Price)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	query.Close()
+	return &product, nil
+}
